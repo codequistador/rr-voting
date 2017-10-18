1638ee43fffee0cd701306916bdda30c362a9d23
Justin Daining <justin@justindaining.com>
2017-10-17 21:56:30 -0600

    up to voting

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,58 +1,58 @@
-import {expect} from 'chai';
-import {List, Map} from 'immutable';
+import {expect} from 'chai'
+import {List, Map} from 'immutable'
 
 describe('immutability', () => {
 
   describe('A Number', () => {
 
     function increment(currentState) {
-      return currentState + 1;
+      return currentState + 1
     }
 
     it('is immutable', () => {
-      let state = 42;
-      let nextState = increment(state);
+      let state = 42
+      let nextState = increment(state)
 
-      expect(nextState).to.equal(43);
-      expect(state).to.equal(42);
-    });
+      expect(nextState).to.equal(43)
+      expect(state).to.equal(42)
+    })
 
-  });
+  })
 
   describe('A List', () => {
 
     function addMovie(currentState, movie) {
-      return currentState.push(movie);
+      return currentState.push(movie)
     }
 
     it('is immutable', () => {
-      let state = List.of('Trainspotting', '28 Days Later');
-      let nextState = addMovie(state, 'Sunshine');
+      let state = List.of('Trainspotting', '28 Days Later')
+      let nextState = addMovie(state, 'Sunshine')
 
       expect(nextState).to.equal(List.of(
         'Trainspotting',
         '28 Days Later',
         'Sunshine'
-      ));
+      ))
       expect(state).to.equal(List.of(
         'Trainspotting',
         '28 Days Later'
-      ));
-    });
+      ))
+    })
 
-  });
+  })
 
   describe('A Tree', () => {
 
     function addMovie(currentState, movie) {
-      return currentState.update('movies', movies => movies.push(movie));
+      return currentState.update('movies', movies => movies.push(movie))
     }
 
     it('is immutable', () => {
       let state = Map({
         movies: List.of('Trainspotting', '28 Days Later')
-      });
-      let nextState = addMovie(state, 'Sunshine');
+      })
+      let nextState = addMovie(state, 'Sunshine')
 
       expect(nextState).to.equal(Map({
         movies: List.of(
@@ -60,15 +60,15 @@ describe('immutability', () => {
           '28 Days Later',
           'Sunshine'
         )
-      }));
+      }))
       expect(state).to.equal(Map({
         movies: List.of(
           'Trainspotting',
           '28 Days Later'
         )
-      }));
-    });
+      }))
+    })
 
-  });
+  })
 
-});
+})
